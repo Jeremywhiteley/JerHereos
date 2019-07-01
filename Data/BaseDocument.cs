7e3b35d90858837472a6a574cc9c48e76ce58ccf
Jeremy Whiteley <jeremy@medtexter.com>
2019-07-01 11:42:39 -0700

    Initial check-in of module Heroes

---
+++ Data/BaseDocument.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Heroes.Data
+{
+    public abstract class BaseDocument: IDocument
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("object")]
+        public abstract string Object { get; }
+
+        [JsonProperty("created_on")]
+        public DateTime CreatedOn { get; set; }
+
+        public BaseDocument()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreatedOn = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Domain/Models/HeroAddOptions.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Heroes.Domain.Models
+{
+    public class HeroAddOptions
+    {
+        [Required]
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("powers")]
+        public string[] Powers { get; set; }
+
+        [JsonProperty("affiliations")]
+        public string[] Affiliations { get; set; }
+
+        [JsonProperty("notes")]
+        public string Notes { get; set; }
+   }
+}
